d3db09afd9d37d091b52cd1874d5713963ebc73b
papadi2004 <papadi2004@gmail.com>
2015-03-16 12:06:58 +0100

    first commit

---
+++ js/javas.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+				$('.first').click(function() {
+					$('.first').toggleClass('one');
+					});
+
+				$('.second').click(function() {
+					$('.second').toggleClass('two');
+					});
+
+				$('.third').click(function() {
+					$('.third').toggleClass('three');
+					});
+
+				$('.fourth').click(function() {
+					$('.fourth').toggleClass('four');
+					});
+
+				$('.fifth').click(function() {
+					$('.fifth').toggleClass('five');
+					});
+
+				$('.sixth').click(function() {
+					$('.sixth').toggleClass('six');
+					});
+
+				$('.seventh').click(function() {
+					$('.seventh').toggleClass('seven');
+					});
+
+				$('.eight').click(function() {
+					$('.eight').toggleClass('eighth');
+					});
+
+				$('.nineth').click(function() {
+					$('.nineth').toggleClass('nine');
+					});
+
+				$('.tenth').click(function() {
+					$('.tenth').toggleClass('ten');
+					});
+			});
